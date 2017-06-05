394aea603d3d3ca48f8d7e961101e9aedf6a9ee4
Bruno Viana <bbviana@gmil.com>
2017-06-04 22:12:03 -0300

    corrigindo copy arquivo jar to folder

--- src/main/java/br/com/viana/money/SantanderFetcher.groovy
+++ src/main/java/br/com/viana/money/SantanderFetcher.groovy
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component
 
 import java.nio.file.Files
 
-import static java.nio.file.StandardCopyOption.COPY_ATTRIBUTES
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING
 import static java.util.concurrent.TimeUnit.MINUTES
 
@@ -48,12 +47,11 @@ class SantanderFetcher {
     }
 
     private extractToTemp(String fileName) {
-        def resource = this.class.classLoader.getResource("phantomjs/${fileName}")
-        def source = new File(resource.path)
+        def source = this.class.classLoader.getResource("phantomjs/${fileName}").openStream()
 
         new File("/tmp/money").mkdir()
         def dest = new File("/tmp/money/${fileName}")
 
-        Files.copy(source.toPath(), dest.toPath(), REPLACE_EXISTING, COPY_ATTRIBUTES)
+        Files.copy(source, dest.toPath(), REPLACE_EXISTING)
     }
 }
